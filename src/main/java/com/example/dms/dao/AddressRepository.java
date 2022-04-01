3cc28484590dc5ac0e8010c0816220b9b0e93276
bavarac93 <spicamuharem@gmail.com>
2022-04-01 19:21:08 +0200

    Changes

--- src/main/java/com/example/dms/dao/AddressRepository.java
+++ src/main/java/com/example/dms/dao/AddressRepository.java
@@ -1,10 +1,11 @@
 package com.example.dms.dao;
 
+
 import com.example.dms.model.AddressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AddressRepository extends JpaRepository <AddressEntity, Long> {
+public interface AddressRepository extends JpaRepository<AddressEntity, Long> {
 
 }
