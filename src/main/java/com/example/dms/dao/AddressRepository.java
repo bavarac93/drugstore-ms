5c5df6d4aa125569643a2c2036ea506955748f20
bavarac93 <spicamuharem@gmail.com>
2022-04-01 19:12:09 +0200

    PostgreSQL connected

---
+++ src/main/java/com/example/dms/dao/AddressRepository.java
@@ -0,0 +1,10 @@
+package com.example.dms.dao;
+
+import com.example.dms.model.AddressEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository <AddressEntity, Long> {
+
+}
--- src/main/java/com/example/dms/model/AddressEntity.java
+++ src/main/java/com/example/dms/model/AddressEntity.java
@@ -1,14 +1,32 @@
 package com.example.dms.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table (name = "address")
 public class AddressEntity {
+
     @Id
-    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String buildingNumber;
+    private String street;
+    private String city;
+    private int postcode;
+    private String country;
+
+    public AddressEntity( ) {
+
+    }
+
+    public AddressEntity(Long id, String buildingNumber, String street, String city, int postcode, String country) {
+        this.id = id;
+        this.buildingNumber = buildingNumber;
+        this.street = street;
+        this.city = city;
+        this.postcode = postcode;
+        this.country = country;
+    }
 
     public Long getId() {
         return id;
@@ -16,5 +34,58 @@ public class AddressEntity {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public void setBuildingNumber(String buildingNumber) {
+        this.buildingNumber = buildingNumber;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(int postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("AddressEntity{");
+        sb.append("id=").append(id);
+        sb.append(", buildingNumber='").append(buildingNumber).append('\'');
+        sb.append(", street='").append(street).append('\'');
+        sb.append(", city='").append(city).append('\'');
+        sb.append(", postcode=").append(postcode);
+        sb.append(", country='").append(country).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
 }
