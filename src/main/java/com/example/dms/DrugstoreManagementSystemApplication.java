97264e471a6d5d1fa4bc4c978107c74d9aca2ef4
bavarac93 <spicamuharem@gmail.com>
2022-04-01 16:38:52 +0200

    Initial commit

---
+++ src/main/java/com/example/dms/DrugstoreManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugstoreManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugstoreManagementSystemApplication.class, args);
+	}
+
+}
