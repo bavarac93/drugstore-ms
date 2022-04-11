6951c4e579566a1f2b42850cc21d149add88ad03
bavarac93 <spicamuharem@gmail.com>
2022-04-11 08:43:41 +0200

    ItemEntity added

---
+++ src/main/java/com/example/dms/model/ItemEntity.java
@@ -0,0 +1,178 @@
+package com.example.dms.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table (name = "item")
+public class ItemEntity {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long itemId;
+    private Long brandId;
+    private Long supplierId;
+    private String productType;
+    private Long price;
+    private Long quantity;
+    private Long sold;
+    private Long available;
+    private Long description;
+    private String expiryDate;
+    private LocalDateTime createdAt;
+    private LocalDateTime createdBy;
+    private LocalDateTime modifiedAt;
+    private LocalDateTime modifiedBy;
+
+    public ItemEntity() {
+    }
+
+    public ItemEntity(Long itemId, Long brandId, Long supplierId, String productType, Long price, Long quantity, Long sold, Long available, Long description, String expiryDate, LocalDateTime createdAt, LocalDateTime createdBy, LocalDateTime modifiedAt, LocalDateTime modifiedBy) {
+        this.itemId = itemId;
+        this.brandId = brandId;
+        this.supplierId = supplierId;
+        this.productType = productType;
+        this.price = price;
+        this.quantity = quantity;
+        this.sold = sold;
+        this.available = available;
+        this.description = description;
+        this.expiryDate = expiryDate;
+        this.createdAt = createdAt;
+        this.createdBy = createdBy;
+        this.modifiedAt = modifiedAt;
+        this.modifiedBy = modifiedBy;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public Long getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Long brandId) {
+        this.brandId = brandId;
+    }
+
+    public Long getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(Long supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    public Long getSold() {
+        return sold;
+    }
+
+    public void setSold(Long sold) {
+        this.sold = sold;
+    }
+
+    public Long getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Long available) {
+        this.available = available;
+    }
+
+    public Long getDescription() {
+        return description;
+    }
+
+    public void setDescription(Long description) {
+        this.description = description;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(LocalDateTime createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(LocalDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+
+    public LocalDateTime getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(LocalDateTime modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ItemEntity{");
+        sb.append("itemId=").append(itemId);
+        sb.append(", brandId=").append(brandId);
+        sb.append(", supplierId=").append(supplierId);
+        sb.append(", productType='").append(productType).append('\'');
+        sb.append(", price=").append(price);
+        sb.append(", quantity=").append(quantity);
+        sb.append(", sold=").append(sold);
+        sb.append(", available=").append(available);
+        sb.append(", description=").append(description);
+        sb.append(", expiryDate='").append(expiryDate).append('\'');
+        sb.append(", createdAt=").append(createdAt);
+        sb.append(", createdBy=").append(createdBy);
+        sb.append(", modifiedAt=").append(modifiedAt);
+        sb.append(", modifiedBy=").append(modifiedBy);
+        sb.append('}');
+        return sb.toString();
+    }
+}
