7d6da660421306e9dc085d9fb98b33625179ff93
bavarac93 <spicamuharem@gmail.com>
2022-04-19 18:43:18 +0200

    validations done

--- src/main/java/com/example/dms/dto/SupplierRequest.java
+++ src/main/java/com/example/dms/dto/SupplierRequest.java
@@ -2,16 +2,29 @@ package com.example.dms.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import javax.validation.constraints.*;
 import java.time.LocalDateTime;
 
 public class SupplierRequest {
+    @NotEmpty(message = "Supplier must contain name.")
+    @NotBlank(message = "Supplier must contain name.")
+    @Pattern(regexp = "[a-zA-Z]+", message = "Supplier name can not contain special characters and numbers.")
     private String supplierName;
+
+    @NotBlank(message = "The supplier must include the description.")
+    @NotEmpty(message = "The supplier must include the description.")
+    @Size(min = 10, max = 250, message = "Description must be between 10 and 250 characters.")
+    @Pattern(regexp = "[a-zA-Z\\d#%'*/<()>:`;,!& .?_]+")
     private String supplierDesc;
 
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @PastOrPresent(message = "The date must be in the past or present.")
     private LocalDateTime contractSigned;
+
+    @Future(message = "The date must be in the future or present. Otherwise, the contract is expired.")
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime contractExpires;
+
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
 
