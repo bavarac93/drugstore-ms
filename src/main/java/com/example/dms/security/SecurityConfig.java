ed8fb8d9496bf0234d34cdfb2d7ec6b83430e741
bavarac93 <spicamuharem@gmail.com>
2022-05-09 15:54:46 +0200

    JWT, roles and users work

--- src/main/java/com/example/dms/security/SecurityConfig.java
+++ src/main/java/com/example/dms/security/SecurityConfig.java
@@ -1,20 +1,22 @@
 package com.example.dms.security;
 
 import com.example.dms.security.filter.CustomAuthenticationFilter;
+import com.example.dms.security.filter.CustomAuthorizationFilter;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import java.util.Objects;
 
 @Configuration
 @EnableWebSecurity
@@ -23,30 +25,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public SecurityConfig(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.userDetailsService = userDetailsService;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    public SecurityConfig(final UserDetailsService userDetailsService, final BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userDetailsService = Objects.requireNonNull(userDetailsService, "userDetailsService cannot be null");
+        this.bCryptPasswordEncoder = Objects.requireNonNull(bCryptPasswordEncoder, "CryptPasswordEncoder cannot be nnull");
     }
 
     @Override
-    protected void configure(@NotNull AuthenticationManagerBuilder auth) throws Exception {
+    protected void configure(@NotNull final AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
 
     @Override
-    protected void configure(@NotNull HttpSecurity http) throws Exception {
+    protected void configure(@NotNull final HttpSecurity http) throws Exception {
+        http.csrf().disable();
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
         customAuthenticationFilter.setFilterProcessesUrl("/security/login");
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().antMatchers("/security/login/**", "/security/token/refresh/**").permitAll();
-        http.authorizeRequests().antMatchers(GET, "/security/**").permitAll();
-        http.authorizeRequests().antMatchers(POST, "/security/**").permitAll();
-        http.authorizeRequests().antMatchers(GET, "/role/**").permitAll();
-        http.authorizeRequests().antMatchers(POST, "/role/**").permitAll();
-//        http.authorizeRequests().antMatchers(POST, "/api/user/add-role-to-user/**").permitAll();
-        http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers("/security/login", "/security/token/refresh/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/security/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/security/user/**").hasAuthority("ROLE_ADMIN");
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/security/user/**").hasAuthority("ROLE_SUPER_ADMIN");
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/role/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/role/**").hasAuthority("ROLE_ADMIN");
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/role/**").hasAuthority("ROLE_SUPER_ADMIN");
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
--- src/main/java/com/example/dms/security/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/example/dms/security/filter/CustomAuthenticationFilter.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -21,8 +22,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -54,21 +53,20 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
         String accessToken = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 100))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
                 .withIssuer(request.getRequestURL().toString())
                 .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(algorithm);
 
         String refreshToken = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 100))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
                 .withIssuer(request.getRequestURL().toString())
                 .sign(algorithm);
-
         Map<String, String> tokens = new HashMap<>();
         tokens.put("access_token", accessToken);
         tokens.put("refresh_token", refreshToken);
-        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         new ObjectMapper().writeValue(response.getOutputStream(), tokens);
     }
 }
