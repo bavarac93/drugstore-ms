c573093251386f548c5e569e77bc370874859f56
Muharem <spicamuharem@gmail.com>
2022-08-13 12:56:34 +0200

    flyway

--- src/main/resources/db/migration/V10__create_users.sql
+++ src/main/resources/db/migration/V10__create_users.sql
@@ -8,12 +8,12 @@ CREATE TABLE IF NOT EXISTS public.users_entity
     name character varying(255) COLLATE pg_catalog."default",
     password character varying(255) COLLATE pg_catalog."default",
     username character varying(255) COLLATE pg_catalog."default",
-    CONSTRAINT users_pkey PRIMARY KEY (id)
+    CONSTRAINT users_entity_pkey PRIMARY KEY (id)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.users
+ALTER TABLE IF EXISTS public.users_entity
     OWNER to postgres;
 
 CREATE SEQUENCE users_entity_sequence START WITH 1 INCREMENT BY 1;
