25817e2080c715ff7f9475c2d67e03e42b366bfe
Muharem <spicamuharem@gmail.com>
2022-08-13 08:58:54 +0200

    upd

--- src/main/resources/db/migration/V11__add_roles_to_users.sql
+++ src/main/resources/db/migration/V11__add_roles_to_users.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS public.users_roles
 (
     users_id bigint NOT NULL,
     roles_id bigint NOT NULL,
-    CONSTRAINT fk7v417qhe0i2m9h8njggvciv00 FOREIGN KEY (users_id)
+    CONSTRAINT FKqhv300wni4ac56vwbqbeqa6fi FOREIGN KEY (users_id)
         REFERENCES public.users (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
